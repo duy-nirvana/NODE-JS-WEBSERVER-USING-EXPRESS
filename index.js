2612769cad969c0bbd1791a42947e11b018295ff
Duy Tran <54000074+duy-nirvana@users.noreply.github.com>
2020-02-27 16:35:34 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express');
-const app = express();
 
+const userRoute = require('./routes/user.route');
 const port = 3020;
 
-app.get('/', (request, response) => {
-    response.send('<h1>Hello, coders X</h1>');
+const app = express();
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        name: 'Hime-San'
+    });
 });
+
+app.use('/users', userRoute);
+
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));

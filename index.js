f824e51abb513ae7e09ccf0b7c76c211eda4dca1
Duy <trankhanhduy8599@gmail.com>
2020-03-01 23:14:55 +0700

    CSRF attack

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ require('dotenv').config();
 console.log(process.env.SESSION_SECRET);
 
 const express = require('express');
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
+const csurf = require('csurf')
 
 const userRoute = require('./routes/user.route');
 const productRoute = require('./routes/product.route');
 const authRoute = require('./routes/auth.route');
+const cartRoute = require('./routes/cart.route');
+const transferRoute = require('./routes/transfer.route');
 
 const authMiddleware = require('./middlewares/auth.middleware');
+const sessionMiddleware = require('./middlewares/session.middleware');
 
 const port = 3020;
 
@@ -19,6 +23,8 @@ app.set('views', './views');
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 app.use(cookieParser(process.env.SESSION_SECRET));
+app.use(sessionMiddleware);
+app.use(csurf({ cookie: true }));
 
 app.use(express.static('public'))
 
@@ -31,5 +37,7 @@ app.get('/', (req, res) => {
 app.use('/users', authMiddleware.requireAuth, userRoute);
 app.use('/products', productRoute);
 app.use('/auth', authRoute);
+app.use('/cart', cartRoute);
+app.use('/transfer', authMiddleware.requireAuth, transferRoute);
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
